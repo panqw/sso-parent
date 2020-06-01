ad48b271d3808dee99748aacb61f23e922a4c7da
panqiwen <452624496@qq.com>
2020-06-01 21:24:41 +0800

    商品的控制器

---
+++ sso-goods/src/main/java/com/sso/goods/controller/TbBrandController.java
@@ -0,0 +1,20 @@
+package com.sso.goods.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 品牌表 前端控制器
+ * </p>
+ *
+ * @author panqw
+ * @since 2020-06-01
+ */
+@RestController
+@RequestMapping("/goods/tb-brand")
+public class TbBrandController {
+
+}
